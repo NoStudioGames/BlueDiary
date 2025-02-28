14121f94fb81a7ac365d96ab8df5df298890555c
ilkin alizade <74135917+TheIlkinAlizade@users.noreply.github.com>
2025-02-28 18:42:51 +0400

    plantv1.0

--- Blue Diary/Assets/Scripts/Systems/Item.cs
+++ Blue Diary/Assets/Scripts/Systems/Item.cs
@@ -3,14 +3,36 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public float speed;
+    Vector3 starPos;
+    Vector3 endPos;
+    public bool inStartPos;
     void Start()
     {
-        
+        starPos = new Vector3(transform.position.x, transform.position.y-1, transform.position.z);
+        endPos = new Vector3(transform.position.x, transform.position.y+1, transform.position.z);
+        transform.position = starPos;
     }
 
     void Update()
     {
-        
+        if(inStartPos)
+        {
+            transform.position = Vector3.Slerp(transform.position, endPos, speed * Time.deltaTime);
+        }
+        if (!inStartPos)
+        {
+            transform.position = Vector3.Slerp(transform.position, starPos, speed * Time.deltaTime);
+        }
+        if(transform.position == starPos)
+        {
+            inStartPos = true;
+            Debug.Log("Start");
+        }
+        if (transform.position == endPos)
+        {
+            inStartPos = false;
+            Debug.Log("End");
+        }
     }
     public void IdleDrop()
     {
